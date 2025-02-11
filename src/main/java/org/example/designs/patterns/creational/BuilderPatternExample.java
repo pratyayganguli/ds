c8a522eaf9a04c63da4ce7a9252cc6c5a7023563
Pratyay Ganguli <pganguli5520@gmail.com>
2025-02-11 15:19:45 +0530

    builder pattern example added

---
+++ src/main/java/org/example/designs/patterns/creational/BuilderPatternExample.java
@@ -0,0 +1,62 @@
+package org.example.designs.patterns.creational;
+
+
+
+/**
+ * *
+ * * @author Pratyay
+ */
+
+public class BuilderPatternExample {
+
+}
+
+class DatabaseCredentials {
+
+    private int port;
+    private String username;
+    private String host;
+    private String password;
+
+    private DatabaseCredentials databaseCredentials;
+
+    private DatabaseCredentials() {
+        databaseCredentials = new DatabaseCredentials();
+    }
+
+    public DatabaseCredentials setUsername(String username) {
+        this.username = username;
+        return databaseCredentials;
+    }
+
+    public DatabaseCredentials setHost(String host) {
+        this.host = host;
+        return databaseCredentials;
+    }
+
+    public DatabaseCredentials setPassword(String password) {
+        this.password = password;
+        return databaseCredentials;
+    }
+
+    public DatabaseCredentials setPort(int port){
+        this.port = port;
+        return databaseCredentials;
+    }
+
+    public DatabaseCredentials build() {
+        return databaseCredentials;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getHost() {
+        return host;
+    }
+}
