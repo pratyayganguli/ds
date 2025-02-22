5707bc7f2d84b72d7c0757b11e5a8df5e860ce18
Pratyay Ganguli <pganguli5520@gmail.com>
2025-02-22 11:45:08 +0530

    DFS traversal code fixes

--- src/main/java/org/example/datastructures/graphs/traversal/dfs/DFSTraversalExample.java
+++ src/main/java/org/example/datastructures/graphs/traversal/dfs/DFSTraversalExample.java
@@ -4,9 +4,9 @@ package org.example.datastructures.graphs.traversal.dfs;
 import lombok.RequiredArgsConstructor;
 import org.example.datastructures.graphs.Vertex;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
+
 
 /**
  * *
@@ -26,28 +26,37 @@ import java.util.Queue;
 
 class DFSTraversalExample {
     public static void main(String[] args) {
-        Vertex v1 = new Vertex(10);
-        Vertex v2 = new Vertex(20);
-        Vertex v3 = new Vertex(30);
-        Vertex v4 = new Vertex(50);
+        Vertex v1 = new Vertex(1);
+        Vertex v2 = new Vertex(2);
+        Vertex v3 = new Vertex(3);
+        Vertex v4 = new Vertex(7);
+        Vertex v5 = new Vertex(9);
+        Vertex v6 = new Vertex(6);
+        Vertex v7 = new Vertex(5);
+        Vertex v8 = new Vertex(8);
 
-        v1.connect(v2);
-        v2.connect(v3);
-        v4.connect(List.of(v2,v3));
+        v1.connect(List.of(v2, v3));
+        v2.connect(List.of(v4, v5));
+        v3.connect(v6);
+        v6.connect(List.of(v7, v8));
+
         DFSTraversal dfsTraversal = new DFSTraversal();
-        Queue queue = new LinkedList();
-        dfsTraversal.traverse(List.of(v1, v2, v3, v4), queue);
+        List<Integer> list = new ArrayList<>();
+        dfsTraversal.traverse(List.of(v1, v2, v3, v4, v5, v6, v7, v8), list);
         System.out.println("debug!");
+        System.out.println(list);
     }
 }
 
 @RequiredArgsConstructor
 class DFSTraversal {
-    public void traverse(List<Vertex> vertices, Queue queue) {
+    public void traverse(List<Vertex> vertices, List list) {
         for(Vertex vertex: vertices) {
-            queue.add(vertex.getData());
+            if(!list.contains(vertex.getData())) {
+                list.add(vertex.getData());
+            }
             if(!vertex.getConnectedVertices().isEmpty()){
-                traverse(vertex.getConnectedVertices(), queue);
+                traverse(vertex.getConnectedVertices(), list);
             }
         }
     }
