63bb3ed81851737c0b8fe8769f47647ff127cb1d
Pratyay Ganguli <pganguli5520@gmail.com>
2025-03-01 12:18:55 +0530

    solving the ATM LLD question

---
+++ src/main/java/org/example/neetcode/stack_questions/ReversePolishNotation.java
@@ -0,0 +1,16 @@
+package org.example.neetcode.stack_questions;
+
+
+import java.util.Stack;
+
+/**
+ * *
+ * * @author Pratyay
+ */
+class ReversePolishNotation {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.pop();
+    }
+
+}
